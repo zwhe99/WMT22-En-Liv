4d29c8f0be18faecba105c6f74aca7e676d53e6e
Hbenmazi <hezw.tkcw@gmail.com>
2022-09-05 21:44:29 +0800

    add help msg in chang-emb script

--- tools/CMEA/change-emb.py
+++ tools/CMEA/change-emb.py
@@ -2,21 +2,36 @@ import torch
 import torch.nn as nn
 import warnings
 import argparse
+import random
+import os
+
 from src.utils import Dictionary
 
+def seed_everything(seed: int):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="Given final dictionary, emb1, emb2 and the checkpoint file, change the embeddings of the checkpoint."
+                    "If w belongs to neither emb1 nor emb2, use random init. Otherwise, emb2 is preferred."
+    )
     parser.add_argument('--model', type=str, required=True, help="model path")
     parser.add_argument('--emb1', type=str, required=True, help="embedding1 path (aligned emb)")
     parser.add_argument('--emb2', type=str, required=True, help="embedding2 path (original emb)")
-    parser.add_argument('--d', type=str, required=True, help="final dict path")
+    parser.add_argument('--dict', type=str, required=True, help="final dictionary path")
     parser.add_argument('--dest', type=str, required=True, help="path to save model")
-    parser.add_argument('--add-mask', action="store_true", help="if add <mask> token")
-    parser.add_argument('--add-tag', action="store_true", help="if add __pivot__ token")
+    parser.add_argument('--add-mask', action="store_true", help="whether to add <mask> token")
+    parser.add_argument('--seed', type=int, default=42, help="random seed")
 
     return parser.parse_args()
 
 def main(args):
+    seed_everything(args.seed)
     model = torch.load(args.model)
 
     data1 = torch.load(args.emb1)
@@ -35,19 +50,14 @@ def main(args):
     emd_dim = emb1.shape[1]
 
     w_li = ['<s>', '<pad>', '</s>', '<unk>']
-    with open(args.d, 'r') as f:
+    with open(args.dict, 'r') as f:
         for line in f:
             w = line.strip().split()[0]
             w_li.append(w)
 
-    if args.add_tag:
-        w_li.append('__pivot__')
-
     if args.add_mask:
         w_li.append('<mask>')
 
-
-    
     new_emb = torch.empty((len(w_li), emd_dim), dtype=model["model"]["encoder.embed_tokens.weight"].dtype)
     for i, w in enumerate(w_li):
         if w in dico2.word2id.keys():
@@ -58,15 +68,11 @@ def main(args):
             warnings.warn(f"{w} is neither in the dico1 nor in the dico2, use random init")
             nn.init.normal_(new_emb[i], mean=0, std=emd_dim**-0.5)
 
-    
     model["model"]["encoder.embed_tokens.weight"] = new_emb
     model["model"]["decoder.embed_tokens.weight"] = new_emb
     model["model"]["decoder.output_projection.weight"] = new_emb
 
     torch.save(model, args.dest)
-
-
-
 
 if __name__ == "__main__":
     args = parse_args()
