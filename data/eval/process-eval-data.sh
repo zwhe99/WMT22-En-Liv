8a8f4276235f8dc03d35efa31013ec905b256571
Hbenmazi <hezw.tkcw@gmail.com>
2022-09-12 11:19:17 +0800

    modify process-eval-data.sh

--- data/eval/process-eval-data.sh
+++ data/eval/process-eval-data.sh
@@ -8,6 +8,9 @@ MAIN_DIR=$(realpath "$SCRIPT_DIR/../..")
 
 N_WORKERS=16
 
+# script to normalize unicode
+NORM_UNICODE=$MAIN_DIR/tools/corpus-tools/norm_unicode.py
+
 # evaluation data dir
 EVAL_DIR=$MAIN_DIR/data/eval
 
@@ -40,6 +43,13 @@ do
         --workers $N_WORKERS
 done
 
+echo "noramlizing the references to NFKC"
+for lng in en et liv lv
+do
+    cat benchmark-test.$lng | python3 $NORM_UNICODE "NFKC" > benchmark-test.nfkc.$lng
+done
+
+
 echo "processing evaluation data (round-trip)"
 python3 $SPM_ENCODE \
     --model $SPM_MODEL \
@@ -61,7 +71,7 @@ python3 $SPM_ENCODE \
 echo "binarize ..."
 fairseq-preprocess \
     --source-lang en \
-    --testpref $EVAL_DIR/wmttest2022.en-liv \
+    --testpref $EVAL_DIR/wmttest2022.spm.en-liv \
     --thresholdsrc 0 \
     --destdir $EVAL_DIR \
     --srcdict $DICT  \
@@ -70,7 +80,7 @@ fairseq-preprocess \
 
 fairseq-preprocess \
     --source-lang liv \
-    --testpref $EVAL_DIR/wmttest2022.liv-en \
+    --testpref $EVAL_DIR/wmttest2022.spm.liv-en \
     --thresholdsrc 0 \
     --destdir $EVAL_DIR \
     --srcdict $DICT  \
